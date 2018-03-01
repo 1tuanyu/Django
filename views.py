70ec155fe968c5583137582fe7f6bfd54f0c5426
1tuanyu <1057727140@qq.com>
2018-03-02 07:44:34 +0800

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,17 @@
+#from django.http import HttpResponse,Http404
+#from django.template.loader import get_template
+#from django.template import Context 用render_to_response节省了导入模版的麻烦
+from django.shortcuts import render_to_response
 import datetime
-from django.http import Http404, HttpResponse
+
+def hello(request):
+    return HttpResponse('hello world!')
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    #t = get_template('current_datetime.html')
+    #html = t.render(Context({'current_date':now}))
+    return render_to_response('current_datetime.html', {'current_date':now})
 
 def hour_ahead(request, offset):
     try:
